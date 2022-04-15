eea9a4047bad6aabee92771a5d5c70e3e3ee7c59
Wesley <chqzy99@gmail.com>
2022-04-15 14:31:05 -0700

    init

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include <TFT_eSPI.h>
+#include <SPI.h>
+TFT_eSPI tft = TFT_eSPI();
+int i = 1;
+
+void setup()
+{
+    tft.init();
+    tft.fillScreen(TFT_BLACK);
+}
+
+void loop()
+{
+  
+  tft.drawString(String(i), 50, 100, 6);
+  sleep(1);
+  i++;
+}
