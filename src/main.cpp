988cf04d874483d854fc780dc30886c7c95827e1
Wesley <chqzy99@gmail.com>
2022-04-21 14:47:12 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,30 +5,24 @@
 #include <SPI.h>
 
 #include <Servo.h>
-int cnt; 
+
+int cnt = 0;
 
 TFT_eSPI tft = TFT_eSPI();
 
-//const int ADC = 26;
- //const int SDA = 21;
- //const int SCL = 22;
-
-
-
 Servo myservo; // create servo object to control a servo
-
 
 // Reset Pin is used for I2C or SPI
 #define CAP1188_RESET  9
 
 // CS pin is used for software or hardware SPI
-#define CAP1188_CS  10
+#define CAP1188_CS  26
 
 // These are defined for software SPI, for hardware SPI, check your 
 // board's SPI pins in the Arduino documentation
-#define CAP1188_MOSI  11
-#define CAP1188_MISO  12
-#define CAP1188_CLK  13
+#define CAP1188_MOSI  32
+#define CAP1188_MISO  33
+#define CAP1188_CLK  25
 
 // For I2C, connect SDA to your Arduino's SDA pin, SCL to SCL pin
 // On UNO/Duemilanove/etc, SDA == Analog 4, SCL == Analog 5
@@ -36,16 +30,16 @@ Servo myservo; // create servo object to control a servo
 // On Mega/ADK/Due, SDA == Digital 20, SCL == Digital 21
 
 // Use I2C, no reset pin!
-Adafruit_CAP1188 cap = Adafruit_CAP1188();
+// Adafruit_CAP1188 cap = Adafruit_CAP1188();
 
 // Or...Use I2C, with reset pin
-//Adafruit_CAP1188 cap = Adafruit_CAP1188(CAP1188_RESET);
+// Adafruit_CAP1188 cap = Adafruit_CAP1188(CAP1188_RESET);
 
 // Or... Hardware SPI, CS pin & reset pin 
 // Adafruit_CAP1188 cap = Adafruit_CAP1188(CAP1188_CS, CAP1188_RESET);
 
 // Or.. Software SPI: clock, miso, mosi, cs, reset
-//Adafruit_CAP1188 cap = Adafruit_CAP1188(CAP1188_CLK, CAP1188_MISO, CAP1188_MOSI, CAP1188_CS, CAP1188_RESET);
+Adafruit_CAP1188 cap = Adafruit_CAP1188(CAP1188_CLK, CAP1188_MISO, CAP1188_MOSI, CAP1188_CS, CAP1188_RESET);
 
 void setup() {
   //pinMode(SDA, INPUT);
@@ -60,55 +54,22 @@ void setup() {
   // Initialize the sensor, if using i2c you can pass in the i2c address
   // if (!cap.begin(0x28)) {
   if (!cap.begin()) {
-    Serial.println("CAP1188 not found");
+    tft.drawString("CAP1188 not found", 20, 50, 1);
     while (1);
   }
-  tft.drawString("CAP1188", 20, 50, 4);
+  tft.drawString("CAP1188 found", 20, 50, 1);
 
 
 }
 
 void loop() {
   uint8_t touched = cap.touched();
-
-  if (touched == 0) {
-    
-    return;
-  }
-  
-  for (uint8_t i = 0; i < 8; i++) {
+  cnt = 0;
+  for(uint8_t i = 0; i < 8; i++) {
     if (touched & (1 << i)) {
       cnt += 1;
-      
     }
   }
-  tft.drawString("C" + String(cnt), 20, 50, 4);
+  tft.drawString("C" + String(cnt), 0, 0, 4);
   delay(1000);
 }
-
-
-// 
-//
-//
-/*
-#include <Arduino.h>
-#include <TFT_eSPI.h>
-#include <SPI.h>
-TFT_eSPI tft = TFT_eSPI();
-int i = 1;
-
-void setup()
-{
-    tft.init();
-    tft.fillScreen(TFT_BLACK);
-}
-
-void loop()
-{
-  // haha
-  // testing this feature
-  tft.drawString(String(i), 50, 100, 6);
-  sleep(1);
-  i++;
-}
-*/
